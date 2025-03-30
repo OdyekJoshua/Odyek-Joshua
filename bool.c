751ed9a40f5735f406888ef86dfa7ce60fe94f69
OdyekJoshua <joshuaodyek90@gmail.com>
2025-03-30 10:43:33 +0300

    Boolean

---
+++ bool.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    bool value = (10>9);//in boolan 1 represent true and 0 represent false.
+    //here we use true and false in small letters.
+    printf("%d",value);
+ 
+    return 0;
+}
