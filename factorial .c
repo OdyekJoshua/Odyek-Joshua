89da4277e30df226d32c23d2acedbb4912dee46d
OdyekJoshua <joshuaodyek90@gmail.com>
2025-05-19 20:06:52 +0300

    Add files via upload
     Using recursion to calculate the factorial
    of a number.
    Please try it out

---
+++ factorial .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fact (int);
+int main()
+{
+int n ,f;
+printf("Enter a number: ");
+scanf("%d" ,&n);
+ f = fact(n);
+printf("factorial = %d",f);
+
+}
+int fact(int n){
+    if (n==0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
